248f41751b45e72bcd719cf4ce992d0d17096815
Emily Ng <ngem3@oocl.com>
2024-11-25 22:02:51 +0800

    feat: get all companies

---
+++ src/test/java/com/oocl/springbootemployee/CompanyControllerTests.java
@@ -0,0 +1,42 @@
+package com.oocl.springbootemployee;
+
+import com.oocl.springbootemployee.model.Company;
+import com.oocl.springbootemployee.model.Employee;
+import com.oocl.springbootemployee.repository.CompanyRepository;
+import com.oocl.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest
+@AutoConfigureMockMvc
+
+@AutoConfigureJsonTesters
+public class CompanyControllerTests {
+    @Autowired
+    private MockMvc client;
+    @Autowired
+    private CompanyRepository companyRepository;
+    @Autowired
+    private JacksonTester<List<Company>> listJson;
+    @Autowired
+    private JacksonTester<Company> json;
+    @Test
+    public void should_retrun_all_employees_when_get_all() throws Exception {
+        List<Company> expected_companies = companyRepository.getAll();
+
+        String companyListJson = client.perform(MockMvcRequestBuilders.get("/companies"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        assertThat(listJson.parse(companyListJson)).usingRecursiveComparison().isEqualTo(expected_companies);
+    }
+}
